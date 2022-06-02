77b10a94201128f2f49d3e6f68d1281a5c67a851
MayceAlazzawi <maynab4@gmail.com>
2022-06-02 14:59:17 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+const p = document.getElementById('text');
+const submitData =  (name, email) => {
+  fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(
+      { name: name,
+        email: email
+      }
+    ),
+  }).then(res => res.json())
+  .then(obj => p.textContent = obj.id)
+    .catch(e => {
+      document.write(e.message); // This will generate an error message  
+    }
+    )
+}
+submitData('mayce', 'nabil')
+
